3e7a5f0f6a79a790ab981f20bd3cf975241438ab
rgoodman <rgoodman@constantcontact.com>
2016-12-13 10:10:48 -0500

    Add abstract messaging function.

--- prawdditions/__init__.py
+++ prawdditions/__init__.py
@@ -1,1 +1,12 @@
 from .const import __version__
+from praw import Reddit, const
+
+def message(self, to, title, body, from_sr=None):
+  data = {'subject': title,
+          'text': body,
+          'to': to}
+  if from_sr:
+    data['from_sr'] = str(from_sr)
+  return self.post(const.API_PATH['compose'], data=data)
+
+Reddit.message = message
