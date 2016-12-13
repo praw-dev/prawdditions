d6f06fe564462ecbe5ae927c24d2c200a905d6db
Bryce Boe <bbzbryce@gmail.com>
2016-12-12 22:29:29 -0800

    Add prawdditions package template

---
+++ README.rst
@@ -0,0 +1,30 @@
+prawdditions
+============
+
+.. image:: https://img.shields.io/pypi/v/prawdditions.svg
+           :alt: Latest prawdditions Version
+           :target: https://pypi.python.org/pypi/prawdditions
+
+
+Prawdditions is a ...
+
+
+Installation
+------------
+
+Install prawdditions using ``pip`` via:
+
+.. code-block:: console
+
+    pip install prawdditions
+
+
+Usage
+-----
+
+The following example demonstrates how to use prawdditions...
+
+.. code-block:: python
+
+   import praw
+   import prawdditions
---
+++ setup.py
@@ -0,0 +1,40 @@
+"""prawdditions setup.py."""
+
+import re
+from codecs import open
+from os import path
+from setuptools import setup
+
+
+PACKAGE_NAME = 'prawdditions'
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as fp:
+    README = fp.read()
+with open(path.join(HERE, PACKAGE_NAME, 'const.py'),
+          encoding='utf-8') as fp:
+    VERSION = re.search("__version__ = '([^']+)'", fp.read()).group(1)
+
+
+setup(name=PACKAGE_NAME,
+      author='NAME',
+      author_email='EMAIL',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: Implementation :: CPython'],
+      description='DESCRIPTION.',
+      install_requires=['requests >=4.0.0, <5.0'],
+      keywords='praw additions',
+      license='Simplified BSD License',
+      long_description=README,
+      packages=[PACKAGE_NAME],
+      url='https://github.com/praw-dev/prawdditions',
+      version=VERSION)
