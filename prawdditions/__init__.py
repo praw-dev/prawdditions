608bb521fe8ade06288e897649b39c8707df9d8b
rgoodman <rgoodman@constantcontact.com>
2016-12-14 09:49:36 -0500

    Update documentation format

--- prawdditions/__init__.py
+++ prawdditions/__init__.py
@@ -1,17 +1,27 @@
 from .const import __version__
 from praw import Reddit, const
 
-"""
-Abstract function for sending out a message via string.
-
-:param to: Destination of the message.
-:param title: The subject line of the message.
-:param body: The body of the message.
-:param from_sr: A Subreddit instance of string to send the message from. By default the message originates from the user.
-:returns: The json response from the server.
-
-"""
 def message(self, to, title, body, from_sr=None):
+  """
+  Abstract function for sending out a message via string.
+
+  :param to: Destination of the message.
+  :param title: The subject line of the message.
+  :param body: The body of the message.
+  :param from_sr: A Subreddit instance of string to send the message from. By default the message originates from the user.
+  :returns: The json response from the server.
+
+  example:
+
+  .. code-block:: python
+           
+    reddit = praw.Reddit(client_id='CLIENT_ID',
+                         client_secret="CLIENT_SECRET", password='PASSWORD',
+                         user_agent='USERAGENT', username='USERNAME')
+    reddit.message('username','title','body')
+
+
+  """
   data = {'subject': title,
           'text': body,
           'to': to}
@@ -19,8 +29,9 @@ def message(self, to, title, body, from_sr=None):
     data['from_sr'] = str(from_sr)
   return self.post(const.API_PATH['compose'], data=data)
 
+
 """
-Attach the message function to the reddit object.
+Attach the message function to the Reddit object.
 Instantiations of the reddit object will now contain the message function.
 """
 Reddit.message = message
