ab4dbeb2ffba84f103c82d07877e6c6344a103b5
Levi Roth <levimroth@gmail.com>
2017-06-20 12:27:22 -0400

    Clean up some flake8 issues in __init__.py

--- prawdditions/__init__.py
+++ prawdditions/__init__.py
@@ -1,33 +1,32 @@
-from .const import __version__
 from praw import Reddit, const
 
+
 def message(self, to, title, body, from_sr=None):
-  """
-  Abstract function for sending out a message via string.
+    """Abstract function for sending out a message via string.
 
-  :param to: Destination of the message.
-  :param title: The subject line of the message.
-  :param body: The body of the message.
-  :param from_sr: A Subreddit instance of string to send the message from. By default the message originates from the user.
-  :returns: The json response from the server.
+    :param to: Destination of the message.
+    :param title: The subject line of the message.
+    :param body: The body of the message.
+    :param from_sr: A Subreddit instance of string to send the message from.
+        By default the message originates from the user.
+    :returns: The json response from the server.
 
-  example:
+    example:
 
-  .. code-block:: python
-           
+    .. code-block:: python
+
     reddit = praw.Reddit(client_id='CLIENT_ID',
                          client_secret="CLIENT_SECRET", password='PASSWORD',
                          user_agent='USERAGENT', username='USERNAME')
     reddit.message('username','title','body')
 
-
-  """
-  data = {'subject': title,
-          'text': body,
-          'to': to}
-  if from_sr:
-    data['from_sr'] = str(from_sr)
-  return self.post(const.API_PATH['compose'], data=data)
+    """
+    data = {'subject': title,
+            'text': body,
+            'to': to}
+    if from_sr:
+        data['from_sr'] = str(from_sr)
+    return self.post(const.API_PATH['compose'], data=data)
 
 
 """
