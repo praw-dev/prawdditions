e40411408aee6e3baf809b5b66bf4a49086dd778
Levi Roth <levimroth@gmail.com>
2017-07-05 19:07:22 -0400

    Add WikiPage.update

--- prawdditions/__init__.py
+++ prawdditions/__init__.py
@@ -1,4 +1,9 @@
+import json
+
+
 from praw import Reddit, const
+from praw.models.reddit.wikipage import WikiPage
+from prawcore.exceptions import Conflict
 
 
 def message(self, to, title, body, from_sr=None):
@@ -29,8 +34,30 @@ def message(self, to, title, body, from_sr=None):
     return self.post(const.API_PATH['compose'], data=data)
 
 
+def update(self, transformation, reason=None):
+    """Safely update a page based on its current content.
+
+    :param transformation: A function taking the previous content as its
+        sole parameter and returning the new content.
+    :param reason: (Optional) The reason for the revision.
+
+    """
+    current_revision = next(self.revisions(limit=1))
+    revision_id = current_revision['id']
+    content = current_revision['page'].content_md
+    new_content = transformation(content)
+    while True:
+        try:
+            self.edit(new_content, reason=reason, previous=revision_id)
+            return
+        except Conflict as conflict:
+            response_body = json.loads(conflict.response.content.decode())
+            new_content = transformation(response_body['newcontent'])
+            revision_id = response_body['newrevision']
+
 """
 Attach the message function to the Reddit object.
 Instantiations of the reddit object will now contain the message function.
 """
 Reddit.message = message
+WikiPage.update = update
